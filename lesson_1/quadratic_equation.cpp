000bbda8628f760c7a25cb812d3a9bf71ea426ec
Lugovtsov Gleb <galera951@gmail.com>
2022-09-06 10:13:01 +0300

    Solver complete

---
+++ lesson_1/quadratic_equation.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+// Function which calculate the discriminant
+double discriminantCalculator(double a, double b, double c) {
+  double discriminant;
+  discriminant = b * b - 4 * a * c;
+
+  return discriminant;
+}
+
+int main() {
+  // Constants
+  const size_t FIELD_WIDTH = 19;
+
+  // Declaring variables
+  double a;
+  double b;
+  double c;
+  double x1;
+  double x2;
+  double discriminant;
+
+  // Program welcome and input coefficients
+  cout << "Welcome to quadratic equation solver!" << endl
+    << "Please, enter the coefficients of eqution: " << endl
+    << "a = ";
+  cin >> a;
+
+  cout << "b = ";
+  cin >> b;
+
+  cout << "c = ";
+  cin >> c;
+
+  cout << endl;
+
+  // Calculate the discriminant
+  discriminant = discriminantCalculator(a, b, c);
+
+  // Calculate and output the solution
+  if (discriminant < 0) {
+    // Case with no solutions
+    cout << "There are no solutions!" << endl;
+
+  } else if (discriminant == 0) {
+    // Case with one solution
+    x1 = (-b + sqrt(discriminant)) / (2 * a);
+
+    cout << "There are one solution:" << endl
+      << "x = " << setw(FIELD_WIDTH) << right << x1 << endl;
+
+  } else {
+    // Case with two solutions
+    x1 = (-b + sqrt(discriminant)) / (2 * a);
+    x2 = (-b - sqrt(discriminant)) / (2 * a);
+
+    cout << "There are two solutions:" << endl
+      << "x1 = " << setw(FIELD_WIDTH) << right << x1 << endl
+      << "x2 = " << setw(FIELD_WIDTH) << right << x2 << endl;
+  };
+
+  // Finish program
+  return EXIT_SUCCESS;
+}
