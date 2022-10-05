eb8b065c9874aa7dfc9a44a4b348726606b3f057
Lugovtsov Gleb <galera951@gmail.com>
2022-10-05 17:19:50 +0300

    working on second task

---
+++ lesson_5/2-struct-link.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+
+
+struct Lecture;
+
+struct Student {
+  std::string full_name() const {
+    return _name + ' ' + _surname;
+  }
+
+  std::string _name;
+  std::string _surname;
+  std::string group;
+  std::vector<Lecture> list_of_lectures;
+};
+
+struct Lecture {
+  void add_student(Student& student) {
+    int index = student_index(student);
+    if (index != -1) {
+      std::cerr << student.full_name() << " already at a " << subject << " lectures.\n" << std::endl;
+    } else {
+      student.list_of_lectures.push_back(*this);
+      list_of_students.push_back(student);
+    }
+  }
+
+  void kick_student(Student& student) {
+    int index = student_index(student);
+    if (index == -1) {
+      std::cerr << student.full_name() << " doesn't aattend " << subject << " lectures.\n" << std::endl;
+    } else {
+      for (size_t i = 0; i < student.list_of_lectures.size(); i++) {
+        if (&student.list_of_lectures.at(i) == this) {
+          student.list_of_lectures.erase(std::begin(student.list_of_lectures) + i);
+          break;
+        }
+      }
+      list_of_students.erase(std::begin(list_of_students) + index);
+    }
+  }
+
+  int student_index(Student const& student) const {
+    for (size_t i = 0; i < list_of_students.size(); i++) {
+      if (&list_of_students.at(i) == &student) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+
+  void print_info() {
+    const int width = 10;
+    std::cout << std::setw(width) << std::left << "Subject:" << '\n'
+              << std::setw(width) << std::left << "Abstract:";
+  }
+
+  std::string subject;
+  std::string abstract;
+  std::vector<Student> list_of_students;
+};
+
+int main(int argc, char const *argv[]) {
+  std::vector<Student> mipt_student{
+    {"Ivan", "Shvydkiy", "B01-201"},
+    {"Maxim", "Kotin", "B04-005"},
+    {"Petr", "Kovalsky", "M02-102"},
+    {"Albina", "Morosheva", "B03-203"},
+    {"Ekaterina", "Zaharova", "B04-107"},
+  };
+
+  std::vector<Lecture> mipt_lectures{
+    {"Mathematical analysis", "Some math quantors"},
+    {"General physics", "Some physics stuff"},
+    {"Informatics", "A lot of programming"},
+    {"English", "Learn new vocabulary and grammar"},
+    {"PE", "The most important subject at MIPT"},
+  };
+
+  mipt
+  return 0;
+}
