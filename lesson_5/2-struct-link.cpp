2fd0094bf8bcce64f250e1c0ab94aca05cae0978
Lugovtsov Gleb <galera951@gmail.com>
2022-10-05 21:24:16 +0300

    Repair second task

--- lesson_5/2-struct-link.cpp
+++ lesson_5/2-struct-link.cpp
@@ -11,22 +11,14 @@ struct Student {
     return _name + ' ' + _surname;
   }
 
-  void print_info() const {
-    const int width = 14;
-    std::cout << std::setw(width) << std::left << "Full name:" << full_name() << '\n'
-              << std::setw(width) << std::left << "Group:" << group << '\n'
-              << std::setw(width) << std::left << "Lectures:" << 'n';
-    for (Lecture const& lecture: list_of_lectures) {
-      std::cout << lecture.subject() << '\n';
-    }
-    std::cout << std::endl;
-  }
+  void print_info();
 
   std::string _name;
   std::string _surname;
   std::string group;
-  std::vector<Lecture> list_of_lectures;
+  std::vector<Lecture*> list_of_lectures;
 };
+
 
 struct Lecture {
   void add_student(Student& student) {
@@ -34,8 +26,8 @@ struct Lecture {
     if (index != -1) {
       std::cerr << student.full_name() << " already at a " << _subject << " lectures.\n" << std::endl;
     } else {
-      student.list_of_lectures.push_back(*this);
-      list_of_students.push_back(student);
+      student.list_of_lectures.push_back(this);
+      list_of_students.push_back(&student);
     }
   }
 
@@ -45,7 +37,7 @@ struct Lecture {
       std::cerr << student.full_name() << " doesn't attend " << _subject << " lectures.\n" << std::endl;
     } else {
       for (size_t i = 0; i < student.list_of_lectures.size(); i++) {
-        if (&student.list_of_lectures.at(i) == this) {
+        if (student.list_of_lectures.at(i) == this) {
           student.list_of_lectures.erase(std::begin(student.list_of_lectures) + i);
           break;
         }
@@ -55,11 +47,8 @@ struct Lecture {
   }
 
   int student_index(Student const& student) const {
-    std::cout << &student << ' ' << student.full_name() << '\n';
-
     for (size_t i = 0; i < list_of_students.size(); i++) {
-      std::cout << &list_of_students.at(i) << ' ' << list_of_students.at(i).full_name() << '\n';
-      if (&list_of_students.at(i) == &student) {
+      if (list_of_students.at(i) == &student) {
         return i;
       }
     }
@@ -72,8 +61,8 @@ struct Lecture {
     std::cout << std::setw(width) << std::left << "Subject:" << _subject << '\n'
               << std::setw(width) << std::left << "Abstract:" << _abstract << '\n'
               << std::setw(width) << std::left << "Students:" << '\n';
-    for (Student const& student: list_of_students) {
-      std::cout << '\t' << std::setw(width) << std::left << student.full_name() << '\n';
+    for (Student* student: list_of_students) {
+      std::cout << '\t' << std::setw(width) << std::left << (*student).full_name() << '\n';
     }
     std::cout << std::endl;
   }
@@ -84,11 +73,24 @@ struct Lecture {
 
   std::string _subject;
   std::string _abstract;
-  std::vector<Student> list_of_students;
+  std::vector<Student*> list_of_students;
 };
 
+
+void Student::print_info() {
+  const int width = 14;
+  std::cout << std::setw(width) << std::left << "Full name:" << full_name() << '\n'
+            << std::setw(width) << std::left << "Group:" << group << '\n'
+            << std::setw(width) << std::left << "Lectures:" << '\n';
+  for (Lecture* lecture: list_of_lectures) {
+    std::cout << '\t'<< (*lecture).subject() << '\n';
+  }
+  std::cout << std::endl;
+}
+
+
 int main(int argc, char const *argv[]) {
-  std::vector<Student> mipt_student{
+  std::vector<Student> mipt_students{
     {"Ivan", "Shvydkiy", "B01-201"},
     {"Maxim", "Kotin", "B04-005"},
     {"Petr", "Kovalsky", "M02-102"},
@@ -104,17 +106,15 @@ int main(int argc, char const *argv[]) {
     {"PE", "The most important subject at MIPT"},
   };
 
-  mipt_lectures.at(0).add_student(mipt_student.at(0));
-  mipt_lectures.at(0).add_student(mipt_student.at(0));
-  // mipt_lectures.at(0).add_student(mipt_student.at(3));
-  // mipt_lectures.at(1).add_student(mipt_student.at(0));
-  // mipt_lectures.at(1).add_student(mipt_student.at(1));
-  // mipt_lectures.at(2).add_student(mipt_student.at(2));
-  // mipt_lectures.at(3).add_student(mipt_student.at(3));
-  // mipt_lectures.at(4).add_student(mipt_student.at(4));
-  // mipt_lectures.at(4).add_student(mipt_student.at(0));
+  mipt_lectures.at(0).add_student(mipt_students.at(0));
+  mipt_lectures.at(0).add_student(mipt_students.at(0));
+  mipt_lectures.at(0).add_student(mipt_students.at(3));
+  mipt_lectures.at(0).add_student(mipt_students.at(2));
+  mipt_lectures.at(1).add_student(mipt_students.at(0));
+  mipt_lectures.at(4).add_student(mipt_students.at(0));
 
   mipt_lectures.at(0).print_info();
+  mipt_students.at(0).print_info();
 
   // mipt
   return 0;
