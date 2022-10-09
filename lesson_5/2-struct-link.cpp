d9af143e7e416e0803543b67fbb40f2ea72c2a31
Lugovtsov Gleb <galera951@gmail.com>
2022-10-10 00:26:06 +0300

    fix 5.2

--- lesson_5/2-struct-link.cpp
+++ lesson_5/2-struct-link.cpp
@@ -2,64 +2,61 @@
 #include <string>
 #include <vector>
 #include <iomanip>
+#include <algorithm>
 
 
 struct Lecture;
 
 struct Student {
-  std::string full_name() const {
-    return _name + ' ' + _surname;
+  Student(std::string const& name,  std::string const& surname, std::string const& group):
+    m_name(name),
+    m_surname(surname),
+    m_group(group)
+    {  }
+
+  const auto full_name() const {
+    return m_name + ' ' + m_surname;
   }
 
-  void print_info();
+  void print_info() const;
 
-  std::string _name;
-  std::string _surname;
-  std::string group;
+  int lecture_index(Lecture const& lecture) const {
+    const auto lect_iter = std::find(std::begin(list_of_lectures), std::end(list_of_lectures), &lecture);
+
+    if (lect_iter != std::end(list_of_lectures)) {
+      return lect_iter - std::begin(list_of_lectures);
+    } else {
+      return -1;
+    }
+  }
+
+  std::string m_name;
+  std::string m_surname;
+  std::string m_group;
   std::vector<Lecture*> list_of_lectures;
 };
 
 
 struct Lecture {
-  void add_student(Student& student) {
-    int index = student_index(student);
-    if (index != -1) {
-      std::cerr << student.full_name() << " already at a " << _subject << " lectures.\n" << std::endl;
-    } else {
-      student.list_of_lectures.push_back(this);
-      list_of_students.push_back(&student);
-    }
-  }
-
-  void kick_student(Student& student) {
-    int index = student_index(student);
-    if (index == -1) {
-      std::cerr << student.full_name() << " doesn't attend " << _subject << " lectures.\n" << std::endl;
-    } else {
-      for (size_t i = 0; i < student.list_of_lectures.size(); i++) {
-        if (student.list_of_lectures.at(i) == this) {
-          student.list_of_lectures.erase(std::begin(student.list_of_lectures) + i);
-          break;
-        }
-      }
-      list_of_students.erase(std::begin(list_of_students) + index);
-    }
-  }
+  Lecture(std::string const& subject, std::string const& abstract):
+    m_subject(subject),
+    m_abstract(abstract)
+    {  }
 
   int student_index(Student const& student) const {
-    for (size_t i = 0; i < list_of_students.size(); i++) {
-      if (list_of_students.at(i) == &student) {
-        return i;
-      }
-    }
+    const auto stud_iter = std::find(std::begin(list_of_students), std::end(list_of_students), &student);
 
-    return -1;
+    if (stud_iter != std::end(list_of_students)) {
+      return stud_iter - std::begin(list_of_students);
+    } else {
+      return -1;
+    }
   }
 
   void print_info() const {
     const int width = 14;
-    std::cout << std::setw(width) << std::left << "Subject:" << _subject << '\n'
-              << std::setw(width) << std::left << "Abstract:" << _abstract << '\n'
+    std::cout << std::setw(width) << std::left << "Subject:" << m_subject << '\n'
+              << std::setw(width) << std::left << "Abstract:" << m_abstract << '\n'
               << std::setw(width) << std::left << "Students:" << '\n';
     for (Student* student: list_of_students) {
       std::cout << '\t' << std::setw(width) << std::left << (*student).full_name() << '\n';
@@ -67,20 +64,20 @@ struct Lecture {
     std::cout << std::endl;
   }
 
-  std::string subject() const {
-    return _subject;
+  auto subject() const {
+    return m_subject;
   }
 
-  std::string _subject;
-  std::string _abstract;
+  std::string m_subject;
+  std::string m_abstract;
   std::vector<Student*> list_of_students;
 };
 
 
-void Student::print_info() {
+void Student::print_info() const{
   const int width = 14;
   std::cout << std::setw(width) << std::left << "Full name:" << full_name() << '\n'
-            << std::setw(width) << std::left << "Group:" << group << '\n'
+            << std::setw(width) << std::left << "Group:" << m_group << '\n'
             << std::setw(width) << std::left << "Lectures:" << '\n';
   for (Lecture* lecture: list_of_lectures) {
     std::cout << '\t'<< (*lecture).subject() << '\n';
@@ -88,37 +85,57 @@ void Student::print_info() {
   std::cout << std::endl;
 }
 
+void connect(Student& student, Lecture& lecture) {
+  int st_lecture_index = student.lecture_index(lecture);
+  int lc_student_index = lecture.student_index(student);
+
+  if (st_lecture_index == -1) {
+    student.list_of_lectures.push_back(&lecture);
+  }
+  if (lc_student_index == -1) {
+    lecture.list_of_students.push_back(&student);
+  }
+}
+
+void disconnect(Student& student, Lecture& lecture) {
+  int st_lecture_index = student.lecture_index(lecture);
+  int lc_student_index = lecture.student_index(student);
+
+  if (st_lecture_index != -1) {
+    student.list_of_lectures.erase(std::next(std::begin(student.list_of_lectures), st_lecture_index));
+  }
+
+  if (lc_student_index != -1) {
+    lecture.list_of_students.erase(std::next(std::begin(lecture.list_of_students), lc_student_index));
+  }
+}
 
 int main(int argc, char const *argv[]) {
-  Student ivan{"Ivan", "Shvydkiy", "B01-201"};
-  Student maxim{"Maxim", "Kotin", "B04-005"};
-  Student albina{"Albina", "Morosheva", "B03-203"};
+  Student ivan("Ivan", "Shvydkiy", "B01-201");
+  Student maxim("Maxim", "Kotin", "B04-005");
+  Student albina("Albina", "Morosheva", "B03-203");
 
-  Lecture math{"Mathematical analysis", "Some math quantors"};
-  Lecture physics{"General physics", "Some physics stuff"};
-  Lecture infornatics{"Informatics", "A lot of programming"};
-  Lecture english{"English", "Learn new vocabulary and grammar"};
+  Lecture math("Mathematical analysis", "Some math quantors");
+  Lecture physics("General physics", "Some physics stuff");
+  Lecture infornatics("Informatics", "A lot of programming");
+  Lecture english("English", "Learn new vocabulary and grammar");
 
-  math.add_student(ivan);
-  math.add_student(maxim);
-  math.add_student(albina);
-
-  math.add_student(maxim); // OUT: error
+  connect(ivan, math);
+  connect(maxim, math);
+  connect(albina, math);
 
   math.print_info();
 
   std::cout << "=============================="<< '\n';
 
-  physics.add_student(ivan);
-  english.add_student(ivan);
+  connect(ivan, physics);
+  connect(ivan, english);
 
   ivan.print_info(); // OUT: Ivan
 
   std::cout << "=============================="<< '\n';
 
-  physics.kick_student(albina); // OUT: error
-
-  math.kick_student(ivan);
+  disconnect(ivan, math);
 
   math.print_info();
 
