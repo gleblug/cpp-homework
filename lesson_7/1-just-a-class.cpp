22b3597929b66fc3176f5112fcd4a3d9ea03637d
Lugovtsov Gleb <galera951@gmail.com>
2022-10-21 12:44:46 +0300

    7.1 works...

--- lesson_7/1-just-a-class.cpp
+++ lesson_7/1-just-a-class.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <sstream>
 
 
 class Array {
 private:
 
-  std::vector<int>  m_vector;
+  int *             m_data;
   size_t            m_length;
-  int               m_data[];
+  std::vector<int>  m_vector;
 
 public:
 
@@ -18,7 +19,7 @@ public:
   // users constructors
   Array (size_t length);
 
-  Array (const int array[]);
+  Array (std::initializer_list<int> array);
 
   Array (const std::vector<int> & array);
 
@@ -49,78 +50,165 @@ public:
   friend Array operator+ (const Array & lhs, const Array & rhs);
 };
 
-Array::Array()
+Array::Array():
+  m_length(0)
 {
 
 }
 
-Array::Array(size_t length)
+Array::Array(size_t length):
+  m_data(new int [length]),
+  m_length(length),
+  m_vector(std::vector<int>(length))
 {
 
 }
 
-Array::Array(const int array[])
+Array::Array(std::initializer_list<int> array)
 {
-
+  m_length = std::distance(std::begin(array), std::end(array));
+  m_vector = array;
+  m_data = new int [m_length];
+  for (size_t i = 0; i < m_length; i++)
+  {
+    m_data[i] = m_vector[i];
+  }
 }
 
 Array::Array(const std::vector<int> & array)
 {
-
+  m_length = array.size();
+  m_vector = array;
+  m_data = new int [m_length];
+  for (size_t i = 0; i < m_length; i++)
+  {
+    m_data[i] = m_vector[i];
+  }
 }
 
 Array::Array(const Array & other)
 {
-
+  m_length = other.m_length;
+  m_vector = other.m_vector;
+  m_data = new int [m_length];
+  for (size_t i = 0; i < m_length; i++)
+  {
+    m_data[i] = m_vector[i];
+  }
 }
 
 Array::Array(Array && other)
 {
+  m_data = other.m_data;
+  m_length = other.m_length;
+  m_vector = std::move(other.m_vector);
 
+  other.m_data = nullptr;
+  other.m_length = 0;
 }
 
 Array::~Array()
 {
-
+  delete [] m_data;
 }
 
 std::ostream & operator<< (std::ostream & stream, const Array & array)
 {
+  for (size_t i = 0; i < array.m_length; i++) {
+    stream << array.m_data[i];
+  }
 
+  return stream;
 }
 
-std::istream & operator<< (std::istream & stream, Array & array)
+std::istream & operator>> (std::istream & stream, Array & array)
 {
+  std::vector<int> buffer;
+  std::string input;
+  std::stringstream streamInput;
+  int elem;
 
+  std::getline(stream, input);
+
+  streamInput << input;
+
+  // Push every elem from streamInput to array as integer while it's not empty
+  while (streamInput >> elem) {
+    buffer.push_back(elem);
+  }
+
+  array = Array(buffer);
+
+  return stream;
 }
 
 int Array::operator[] (size_t index) const
 {
+  if (index >= m_length) {
+    std::cerr << "Index is bigger than array length." << '\n';
+  }
 
+  return m_data[index];
 }
 
 int & Array::operator[] (size_t index)
 {
+  if (index >= m_length) {
+    std::cerr << "Index is bigger than array length." << '\n';
+  }
 
+  return m_data[index];
 }
 
 Array & Array::operator= (const Array & other)
 {
+  m_length = other.m_length;
+  m_vector = other.m_vector;
+  m_data = new int [m_length];
+  for (size_t i = 0; i < m_length; i++)
+  {
+    m_data[i] = m_vector[i];
+  }
 
+  return (*this);
 }
 
 Array & Array::operator= (Array && other)
 {
+  m_data = other.m_data;
+  m_length = other.m_length;
+  m_vector = std::move(other.m_vector);
 
+  other.m_data = nullptr;
+  other.m_length = 0;
+
+  return (*this);
 }
 
 Array operator+(const Array & lhs, const Array & rhs)
 {
+  size_t length = lhs.m_length + rhs.m_length;
+  Array array(length);
 
+  for (size_t i = 0; i < lhs.m_length; i++) {
+    array.m_data[i] = lhs.m_data[i];
+    array.m_vector[i] = lhs.m_vector[i];
+  }
+
+  for (size_t i = lhs.m_length; i < array.m_length; i++) {
+    array.m_data[i] = rhs.m_data[i - lhs.m_length];
+    array.m_vector[i] = rhs.m_vector[i - lhs.m_length];
+  }
+
+  return array;
 }
 
 
 int main(int argc, char const *argv[]) {
-  /* code */
+  Array a = {1, 2, 3, 4};
+  Array b = {4, 6, 7};
+
+  std::cout << a + b << '\n';
+
   return 0;
 }
