60504dca7b7e0ab593cea61cd3cf6ee8f9ff9100
Lugovtsov Gleb <galera951@gmail.com>
2022-10-27 13:26:43 +0300

    move console graphic project

--- lesson_8/1-figures/main.cpp
+++ lesson_8/1-figures/main.cpp
@@ -1,14 +1,4 @@
-#include <vector>
-#include "FigurePlot.hpp"
-#include "Vec2d.hpp"
-
 int main(int argc, char const *argv[]) {
-
-  FigurePlot figure_plot;
-
-  std::vector<Vec2d> triangle = {Vec2d(1, 1), Vec2d(-2, 1), Vec2d(-2, -1), Vec2d(3, 0)};
-
-  figure_plot.plot2d(triangle);
 
   return 0;
 }
