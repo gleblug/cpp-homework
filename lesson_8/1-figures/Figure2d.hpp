2b03ff1fb5d7e86e128913d14d5057c52c784ccd
Lugovtsov Gleb <galera951@gmail.com>
2022-10-29 00:10:56 +0300

    work on figure

--- lesson_8/1-figures/Figure2d.hpp
+++ lesson_8/1-figures/Figure2d.hpp
@@ -1,29 +1,73 @@
 #pragma once
+#include<vector>
+#include<string>
+#include<iostream>
+
+
+enum class Exceptions {
+  invalid_number_of_points
+};
+
+void error(Exceptions exception) {
+  std::string error_message = "Unknown error.";
+  switch (exception) {
+    case Exceptions::invalid_number_of_points:
+      error_message = "Invalid number of points!";
+      break;
+  }
+  std::cerr << "ERROR: " << error_message << std::endl;
+  exit(EXIT_FAILURE);
+}
 
 // Base figure class
 class Figure2d {
-private:
-
 public:
-  Figure2d ();
-  virtual ~Figure2d ();
+  Figure2d() = default;
+
+  virtual double get_perimeter() const;
+
+  virtual double get_area() const;
+
+  virtual void print_info() const;
+
+  virtual std::pair<double, double> get_barycenter() const;
+
+  virtual void extent(const double scale);
+
+  virtual void rotate(const double degree);
+
+  virtual ~Figure2d() = default;
 };
 
 // Figure -> Polygon class
 class Polygon: public Figure2d {
-private:
-
 public:
-  Polygon ();
-  virtual ~Polygon ();
+  Polygon(const std::vector<std::pair<double, double>> points):
+    m_points(points) {  };
+
+  double get_perimeter() const override;
+
+  double get_area() const override;
+
+  void print_info() const override;
+
+  std::pair<double, double> get_barycenter() const override;
+
+  void extent(const double scale) override;
+
+  void rotate(const double degree) override;
+
+private:
+  std::vector<std::pair<double, double>> m_points;
 };
 
 // Figure -> Polygon -> Triangle class
 class Triangle: public Polygon {
-private:
-
 public:
-  Triangle ();
+  Triangle (const std::vector<std::pair<double, double>> points):
+    Polygon(points) {
+    if (points.size() != 3) error(Exceptions::invalid_number_of_points);
+  }
   virtual ~Triangle ();
 };
 
