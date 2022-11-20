fc8d0be98704928dcd6d8f8ad3f3dea4b140e5dc
Lugovtsov Gleb <lugovtsov.gs@phystech.edu>
2022-11-20 15:13:34 +0300

    fix 10.2

--- lesson_10/2-container-template/Container.hpp
+++ lesson_10/2-container-template/Container.hpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <algorithm>
 #include <memory>
-#include "ContainerIterator.hpp"
 
 template <typename T>
 class Container;
@@ -30,35 +29,35 @@ template <typename T>
 class Container
 {
 public:
-    typedef ContainerIterator<T> iterator;
-    typedef ContainerIterator<const T> const_iterator;
-    
+    using value_type = T;
+    using pointer = T *;
+    using const_pointer = const T *;
+    using reference = T &;
+    using const_reference = const T &;
+    using size_type = std::size_t;
+    using iterator = T *;
+    using const_iterator = const T *;
 
-    explicit Container(size_t size) : m_size(size), m_data(new T[size]) {}
+    explicit Container(size_type size) : m_size(size), m_data(new value_type[size]) {}
 
-    Container(T* const&ptr, size_t size) : m_size(size), m_data(new T[size])
+    Container(const_pointer ptr, size_type size) : m_size(size), m_data(new value_type[size])
     {
         for (size_t i = 0; i < size; i++)
             m_data[i] = ptr[i];
     }
 
-    Container(T* &&ptr, size_t size) : m_size(size), m_data(ptr)
+    Container(std::initializer_list<value_type> &&i_list) : m_size(std::size(i_list)), m_data(new value_type[m_size])
     {
-        ptr = nullptr;
+        std::copy(std::begin(i_list), std::end(i_list), m_data);
     }
 
-    Container(std::initializer_list<T> &&i_list) : m_size(std::size(i_list)), m_data(new T[m_size])
-    {
-        std::copy(std::begin(i_list), std::end(i_list), m_data.get());
-    }
-
-    Container(std::vector<T> const &vector) : m_size(vector.size()), m_data(new T[size])
+    Container(std::vector<value_type> const &vector) : m_size(vector.size()), m_data(new value_type[size])
     {
         for (size_t i = 0; i < m_size; i++)
             m_data[i] = vector[i];
     }
 
-    Container(Container const &other) : m_size(other.m_size), m_data(new T[m_size])
+    Container(Container const &other) : m_size(other.m_size), m_data(new value_type[m_size])
     {
         for (size_t i = 0; i < m_size; i++)
         {
@@ -72,50 +71,55 @@ public:
         other.m_data.release();
     }
 
+    ~Container()
+    {
+        delete[] m_data;
+    }
+
     iterator begin() noexcept
     {
-        return iterator(m_data.get());
+        return m_data;
     }
 
     iterator end() noexcept
     {
-        return iterator(std::next(m_data.get(), m_size));
+        return std::next(m_data, m_size);
     }
 
     const_iterator begin() const noexcept
     {
-        return const_iterator(m_data.get());
+        return m_data;
     }
 
     const_iterator end() const noexcept
     {
-        return const_iterator(std::next(m_data.get(), m_size));
+        return std::next(m_data, m_size);
     }
 
-    const T& operator[](std::size_t ind) const
+    const_reference operator[](size_type ind) const
     {
         if (ind >= m_size)
             throw std::out_of_range("Index out of range: " + std::to_string(ind) + " >= " + std::to_string(m_size));
         return m_data[ind];
     }
 
-    T& operator[](std::size_t ind)
+    reference operator[](size_type ind)
     {
         if (ind >= m_size)
             throw std::out_of_range("Index out of range: " + std::to_string(ind) + " >= " + std::to_string(m_size));
         return m_data[ind];
     }
 
-    size_t size() const noexcept
+    size_type size() const noexcept
     {
         return m_size;
     }
 
-    friend void std::swap<T>(Container<T> &lhs, Container<T> &rhs);
+    friend void std::swap<value_type>(Container<value_type> &lhs, Container<value_type> &rhs);
 
-    Container &resize(size_t new_size);
+    Container &resize(size_type new_size);
 
-    Container &append(T const &value)
+    Container &append(const_reference value)
     {
         resize(m_size + 1);
         m_data[m_size] = value;
@@ -135,24 +139,26 @@ public:
 
     Container &reverse() noexcept
     {
-        std::unique_ptr<T[]> data(new T [m_size]);
+        T *data = new T[m_size];
         for (size_t i = 0; i < m_size; i++)
             data[i] = m_data[i];
         for (size_t i = 0; i < m_size; i++)
             m_data[m_size - i - 1] = data[i];
-        
+
+        delete[] data;
+
         return *this;
     }
 
 private:
-    size_t m_size;
-    std::unique_ptr<T[]> m_data;
+    size_type m_size;
+    value_type *m_data;
 };
 
 template <typename T>
 std::ostream &operator<<(std::ostream &stream, Container<T> const &container)
 {
-    for (auto const &c: container)
+    for (auto const &c : container)
         stream << c << " ";
     return stream;
 }
@@ -161,59 +167,63 @@ template <typename T>
 void std::swap(Container<T> &lhs, Container<T> &rhs)
 {
     size_t lhs_size = lhs.m_size;
-    std::unique_ptr<T[]> lhs_data = lhs.m_data;
+    T *lhs_data = lhs.m_data;
 
     lhs.m_size = rhs.m_size;
     lhs.m_data = rhs.m_data;
 
     rhs.m_size = lhs_size;
     rhs.m_data = lhs_data;
+
+    delete[] lhs_data;
 }
 
 template <typename T>
-Container<T> *std::begin(Container<T> &container)
+typename Container<T>::iterator std::begin(Container<T> &container)
 {
     return container.begin();
 }
 
 template <typename T>
-Container<T> *std::end(Container<T> &container)
+typename Container<T>::iterator std::end(Container<T> &container)
 {
     return container.end();
 }
 
 template <typename T>
-constexpr Container<T> *std::begin(Container<T> const &container) noexcept
-{   
+constexpr typename Container<T>::const_iterator std::begin(Container<T> const &container) noexcept
+{
     return container.begin();
 }
 
 template <typename T>
-constexpr Container<T> *std::end(Container<T> const &container) noexcept
-{   
+constexpr typename Container<T>::const_iterator std::end(Container<T> const &container) noexcept
+{
     return container.end();
 }
 
 template <typename T>
-Container<T> &Container<T>::resize(size_t new_size)
+Container<T> &Container<T>::resize(typename Container<T>::size_type new_size)
 {
-    std::unique_ptr<T[]> data(new T[m_size]);
+    T *data = new T[m_size];
     for (size_t i = 0; i < m_size; i++)
         data[i] = m_data[i];
 
-    m_data = std::unique_ptr<T[]>(new T [new_size]);
+    m_data = new T[new_size];
 
     for (size_t i = 0; i < std::min(m_size, new_size); i++)
         m_data[i] = data[i];
 
     m_size = new_size;
 
+    delete[] data;
+
     return *this;
 }
 
 template <typename T>
 Container<T> &Container<T>::append(Container<T> const &other)
-{   
+{
     size_t old_size = m_size;
     resize(m_size + other.m_size);
     for (size_t i = old_size; i < m_size; i++)
