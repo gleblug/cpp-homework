3225002726a0767243c8f5866244dbe33eeede65
Lugovtsov Gleb <lugovtsov.gs@phystech.edu>
2022-12-12 01:26:16 +0300

    fix maclaurin exp

--- lesson_13/3-Maclaurin-exponent.cpp
+++ lesson_13/3-Maclaurin-exponent.cpp
@@ -3,44 +3,26 @@
 
 inline constexpr double e_v = 2.718281828459045235360287471352;
 
-template <std::size_t n>
-struct Calculate_Factorial
-{
-    static constexpr long long value = Calculate_Factorial<n - 1>::value * n;
-};
-
-template <>
-struct Calculate_Factorial<0>
-{
-    static constexpr long long value = 1;
-};
 
 template <std::size_t n>
-constexpr long long Factorial = Calculate_Factorial<n>::value;
-
-
-template <std::size_t n>
-struct Calculate_Exponent
+constexpr double Calculate_Exponent()
 {
-    static constexpr double value = Calculate_Exponent<n - 1>::value + 1.0 / Factorial<n>;
-};
+    double value = 1;
+    long long factorial = 1;
 
-template <>
-struct Calculate_Exponent<0>
-{
-    static constexpr double value = 1;
-};
-
-template <std::size_t n>
-constexpr double Exponent = Calculate_Exponent<n>::value;
+    for (size_t i = 1; i <= n; i++)
+        value += 1.0f / (factorial *= i);
+    
+    return value;
+}
 
 
 int main(int argc, char const *argv[])
 {
-    constexpr std::size_t accuracy = 20;
-    constexpr double epsilon = 1e-15;
+    constexpr std::size_t accuracy = 18;
+    constexpr double epsilon = 1e-5;
 
-    constexpr double e = Exponent<accuracy>;
+    constexpr double e = Calculate_Exponent<accuracy>();
     
     static_assert(std::abs(e_v - e) < epsilon);
 
