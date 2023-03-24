a3802253a1f67d007dd1c83d86d9a56a1df4ad82
Gleb Lugovtsov <lugovtsov.gs@phystech.edu>
2023-03-24 23:36:49 +0300

    complete 2.06.2

--- lesson-2-06/2-get-a-line/main.cpp
+++ lesson-2-06/2-get-a-line/main.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <iostream>
 #include <string>
 #include <iterator>
 #include <set>
@@ -21,23 +23,38 @@ std::set < std::string > make_random_words(std::size_t N = 1000, std::size_t len
     return words;
 }
 
-void create_file(const std::string &filename)
+void create_file(const std::string &filename, const size_t number_of_words, const size_t word_length)
 {
     std::ofstream fout(filename);
-    std::ostream_iterator<std::string> oit(fout, "\n");
+    std::ostream_iterator<std::string> fout_iter(fout, "\n");
 
-    const auto words = make_random_words();
+    const auto words = make_random_words(number_of_words, word_length);
 
-    copy(cbegin(words), cend(words), oit);
+    copy(cbegin(words), cend(words), fout_iter);
 }
 
 int main(int argc, char const *argv[])
 {
     const std::string filename = "words.txt";
-    if ((!std::filesystem::exists(filename)) || (!std::filesystem::is_regular_file(filename)))
-        create_file(filename);
+    const size_t number_of_words = 1000;
+    const size_t word_length = 10;
 
-    
+    if ((!std::filesystem::exists(filename)) || (!std::filesystem::is_regular_file(filename)))
+        create_file(filename, number_of_words, word_length);
+
+    std::ifstream fin(filename);
+    std::string word;
+    size_t line_number;
+
+    std::cout << "Enter a line number (1-" << number_of_words << "):" << std::endl;
+
+    while (std::cin) 
+    {
+        std::cin >> line_number;
+        fin.seekg((line_number - 1) * (word_length + 1));
+        fin >> word;
+        std::cout << word << std::endl;
+    }
 
     return 0;
 }
